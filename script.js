b355c58de174d053e45f5a9572f11d08f66c46fa
Alexander Portera <aportera3@gmail.com>
2020-10-27 21:27:50 -0400

    defined variables and arrays

--- script.js
+++ script.js
@@ -10,11 +10,20 @@ function writePassword() {
 
 }
 
+// variables
+var specialCharacters 
+var numberCharacters
+var lowerCharacters
+var upperCharacters
 
-var specialChar = " !?@#$%^&*()_+-=[]{}<>;:'`,./\|~"
-var numberChar = "0123456789"
-var lowerCharacters = "abcdefghijklmnopqrstuvwxyz"
-var upperChar = lowerCharacters.toUpperCase()
+// arrays
+var begin = "";
+specialCharacters = ["!", "?", "@", "#", "$", "%", "^", "&", "_", "+", "-", "=", ";", ":", "`", ",", ".", "/", "|"]
+numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+upperCharacters = lowerCharacters.toUpperCase()
+
+
 
 
 
