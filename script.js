e34c993089ee46215274eee79544c0e1c35b7bfa
Alexander Portera <aportera3@gmail.com>
2020-10-28 18:46:58 -0400

    js file

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ var generateBtn = document.querySelector("#generate");
 // }
 
 // variables
-var specialCharacters 
+var specialCharacters
 var numberCharacters
 var lowerCharacters
 var upperCharacters
+var begin
 
 // arrays
-var begin = "";
 specialCharacters = ["!", "?", "@", "#", "$", "%", "^", "&", "_", "+", "-", "=", ";", ":", "`", ",", ".", "/", "|"]
 numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -32,16 +32,35 @@ function writePassword() {
   passwordText.value = password;
 
 
-  var begin = prompt("Please indicate how many characters you want from 8 - 128.");
+  var begin = parseInt(prompt("Please indicate how many characters you want from 8 - 128."));
   if (!begin) {
-    alert ("Please pick a vaild number. Try again.")
+    alert("Please pick a vaild number. Try again.")
   }
 
   else if (begin < 8 || begin > 128) {
-    alert ("Please pick a vaild number. Try again.");
-    begin = prompt ("Please indicate how many characters you want from 8 - 128.");
-  
+    alert("Please pick a vaild number. Try again.");
+    begin = parseInt(prompt("Please indicate how many characters you want from 8 - 128."));
+    specialCharacters = confirm("Do you want include special characters?");
+    numberCharacters = confirm("Do you want to include  numbers?");
+    lowerCharacters = confirm("Do you want to use lower case letters?");
+    upperCharacters = confirm("Do you want to use upper case letters?");
   }
+
+  else {
+    specialCharacters = confirm("Do you want include special characters?");
+    numberCharacters = confirm("Do you want to include  numbers?");
+    lowerCharacters = confirm("Do you want to use lower case letters?");
+    upperCharacters = confirm("Do you want to use upper case letters?");
+  }
+
+  if (!specialCharacters && !numberCharacters && !lowerCharacters && !upperCharacters) {
+    alert("Invald. You need to pick at least one field.")
+    specialCharacters = confirm("Do you want include special characters?");
+    numberCharacters = confirm("Do you want to include  numbers?");
+    lowerCharacters = confirm("Do you want to use lower case letters?");
+    upperCharacters = confirm("Do you want to use upper case letters?");
+  }
+
 
 }
 
